2168f644ce8d08360923334122b86823b615d143
dhruvi618 <dhruvisheth18@gmail.com>
2020-05-28 11:47:49 -0400

    Implement about section with fun fact generator and modify portfolio layout

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -13,16 +13,19 @@
 // limitations under the License.
 
 /**
- * Adds a random greeting to the page.
+ * Adds a random fun fact to the page.
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+function addRandomFunFact() {
+  const facts =
+      ['I have lived in three countries: India, Canada, and the US!', 
+        'I can speak/understand 4 different languages!', 
+        'I love eating Chocolate Chip Cookie Dough ice cream!', 
+        'I have only visited 8 of the 50 US states!'];
 
   // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+  const fact = facts[Math.floor(Math.random() * facts.length)];
 
   // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+  const factContainer = document.getElementById('fact-container');
+  factContainer.innerText = fact;
 }
