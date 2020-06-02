ef06b584056d1a7a1dea59cd9136aa708aa919d2
dhruvi618 <dhruvisheth18@gmail.com>
2020-06-02 17:53:55 +0000

    change messages list declaration from ArrayList to List

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -19,14 +19,14 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
+import java.util.*;
 import com.google.gson.Gson;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
     
-  private ArrayList<String> messages;
+  private List<String> messages;
 
   @Override
   public void init() {
