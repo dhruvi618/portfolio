19f83083abf0b39bc084ed61c50b228bb1bd9247
dhruvi618 <dhruvisheth18@gmail.com>
2020-06-16 21:27:34 +0000

    Optimize finding last event by using Collection.max instead of sorting all elements by end time

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -17,6 +17,7 @@ package com.google.sps;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public final class FindMeetingQuery {
@@ -30,24 +31,22 @@ public final class FindMeetingQuery {
     }
     
     List<TimeRange> eventsSortedByStartTime = new ArrayList<>();
-    List<TimeRange> eventsSortedByEndTime = new ArrayList<>();
 
     for (Event event : events) {
       // Add event to events lists if meeting request contains attendees of the event  
       if (!Collections.disjoint(requiredAttendees, event.getAttendees())) {
         eventsSortedByStartTime.add(event.getWhen());
-        eventsSortedByEndTime.add(event.getWhen());
       }
     }
     Collections.sort(eventsSortedByStartTime, TimeRange.ORDER_BY_START);
-    Collections.sort(eventsSortedByEndTime, TimeRange.ORDER_BY_END);
 
     if (requiredAttendees.isEmpty() || eventsSortedByStartTime.isEmpty()) {
       meetingQueryOptions.add(TimeRange.WHOLE_DAY);
     } else {
       int numberOfEvents = eventsSortedByStartTime.size();
       int firstEventStartTime = eventsSortedByStartTime.get(0).start();
-      int lastEventEndTime = eventsSortedByEndTime.get(numberOfEvents - 1).end();
+      TimeRange lastEventRangeByEndTime = Collections.max(eventsSortedByStartTime, Comparator.comparingInt(TimeRange::end));
+      int lastEventEndTime = lastEventRangeByEndTime.end();
 
       if (firstEventStartTime - TimeRange.START_OF_DAY >= meetingDuration) {
         meetingQueryOptions.add(TimeRange.fromStartEnd(TimeRange.START_OF_DAY, firstEventStartTime, false));
@@ -58,7 +57,7 @@ public final class FindMeetingQuery {
           int currentEventEndTime = eventsSortedByStartTime.get(currentEventIndex).end();
           int nextEventStartTime = eventsSortedByStartTime.get(currentEventIndex+1).start();
           if (currentEventEndTime < nextEventStartTime && nextEventStartTime - currentEventEndTime >= meetingDuration) {
-            meetingQueryOptions.add(TimeRange.fromStartEnd(currentEventEndTime, nextEventStartTime, /* {inclusive}= */ false));
+            meetingQueryOptions.add(TimeRange.fromStartEnd(currentEventEndTime, nextEventStartTime, /* inclusive= */ false));
           }
         }
       }
